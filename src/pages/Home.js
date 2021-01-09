fb576df7022d68b1043e1f4f1974d0df3d53f23c
Your Name <35497915+hexmo@users.noreply.github.com>
2021-01-09 16:31:35 +0545

    Created home page which shows latest games.

---
+++ src/pages/Home.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+// Components
+import Game from '../components/Game';
+
+// Styling and animation
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { loadGames } from '../actions/gamesAction';
+
+const Home = () => {
+	// fetch games
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(loadGames());
+	}, [dispatch]);
+
+	// Get fetched data
+	const { popular, newGames, upComingGames } = useSelector(
+		(state) => state.games,
+	);
+	return (
+		<GameList>
+			<h2>Upcoming Games</h2>
+			<Games>
+				{upComingGames.map((game) => (
+					<Game
+						name={game.name}
+						released={game.released}
+						id={game.id}
+						key={game.id}
+						image={game.background_image}
+					/>
+				))}
+			</Games>
+		</GameList>
+	);
+};
+
+// Styled components and framer motion
+const GameList = styled(motion.div)`
+	padding: 0rem 5rem;
+	h2 {
+		padding: 5rem 0rem;
+	}
+`;
+const Games = styled(motion.div)`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+	min-height: 80vh;
+	grid-column-gap: 3rem;
+	grid-row-gap: 5rem;
+`;
+
+export default Home;
