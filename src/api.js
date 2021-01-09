9effa01cd8733293f1aa824dc91b6fa3a3232461
Your Name <35497915+hexmo@users.noreply.github.com>
2021-01-09 11:42:20 +0545

    2. Create popular games api

---
+++ src/api.js
@@ -0,0 +1,34 @@
+// Base url
+const base_url = 'https://api.rawg.io/api/';
+
+// getting the date
+const getCurrentMonth = () => {
+	const month = new Date().getMonth() + 1;
+	if (month < 10) {
+		return `0${month}`;
+	} else {
+		return month;
+	}
+};
+
+const getCurrentDay = () => {
+	const day = new Date().getDate;
+	if (day < 10) {
+		return `0${day}`;
+	} else {
+		return day;
+	}
+};
+
+// Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// Popular games
+const popular_games = `game?dates=${lastYear},${currentDate}&odering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
